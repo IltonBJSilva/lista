29b0af41bd05c858ab6c0978fa68aaa7407f6013
IltonBJSilva <xlsonplay@gmail.com>
2018-11-14 16:43:31 -0200

    Mecanismo de Adicionar Tarefa colocado

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function AcresentaTarefa(){
-	
+function acresentaTarefa(event){
+	if(event.which == 13 || event.keycode == 13){
+		
+		//Para acresentar tarefa 
+		var tarefa = document.createElement("div");
+		
+		tarefa.id = "TarefaID";
+		
+		//Para acresentar o texto
+		var texto = document.createTextNode(document.getElementById("Criador").value);
+		
+		tarefa.appendChild(texto);
+		
+		//Para acresentar um novo ao corpo o body
+		document.body.appendChild(tarefa);
+		return false;
+	}else{
+		return true;
+	}
+
 }
+
