ee4c53a9d9f9f9e22c4bb4f38fcf97d277c9f7e3
IltonBJSilva <xlsonplay@gmail.com>
2018-11-16 16:53:11 -0200

    Cor na caixa adicionado/No empty add

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 function acresentaTarefa(event){
-	
+
 	var valor = document.getElementById("Criador").value;
-	
-//	if(valor === ""){
-//		alert("Insira um valor");
-//	   } else 
-		if(event.which == 13 || event.keycode == 13  ){
-		
-		//Para acresentar tarefa 
-		var tarefa = document.createElement("div");
-		
-		tarefa.id = "TarefaID";
-		
-		//Para acresentar o texto
-		var texto = document.createTextNode(valor);
-		
-		
-		
-		document.getElementById("Criador").value = "";
-		
-		tarefa.appendChild(texto);
-		
-		//Para acresentar um novo ao corpo o body
-		document.body.appendChild(tarefa);
-		return false;
+
+	//	if(valor === ""){
+	//	alert("Insira um valor");
+	//	  } else 
+	if(event.which == 13 || event.keycode == 13  ){
+		if(valor == ""){
+		   alert("Insira um valor");	
+		   }else{
+			//Para acresentar tarefa 
+			var tarefa = document.createElement("div");
+
+			//dar uma ID para cada div criada
+			tarefa.id = "TarefaID";
+
+			//Para acresentar o texto
+			var texto = document.createTextNode(valor);
+
+
+			//Limpar o campo de tarefas 
+			document.getElementById("Criador").value = "";
+
+			tarefa.appendChild(texto);
+
+			//Para acresentar um novo ao corpo o body
+			document.body.appendChild(tarefa);
+			return false;
+		}
 	}else{
-		return true;
+	return true;
 	}
 
 }
 
 function deletaTarefa(event){
 
-	
-}
 
+}
