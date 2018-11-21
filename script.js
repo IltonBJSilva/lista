452223618f35dfbbbfdbd467a27fd9dc1c45f763
IltonBJSilva <xlsonplay@gmail.com>
2018-11-21 17:59:55 -0200

    Simbolos Adicionados

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
 function acresentaTarefa(event){
 
 	var valor = document.getElementById("Criador").value;
-
-	//	if(valor === ""){
-	//	alert("Insira um valor");
-	//	  } else 
+	
 	if(event.which == 13 || event.keycode == 13  ){
 		if(valor == ""){
 		   alert("Insira um valor");	
-		   }else{
+		 } else {
 			//Para acresentar tarefa 
 			var tarefa = document.createElement("li");
-
-			//dar uma ID para cada div criada
-			tarefa.id = "TarefaID" + valor;
-
-			//Para acresentar o texto
 			var texto = document.createTextNode(valor);
+			  
+			//Para criar um SPAN onde o simbolo sera colocada e Para criar o proprio simbolo
+			var spancheck = document.createElement("SPAN");
+			var check = document.createTextNode("\u2713");
+			spancheck.appendChild(check);	
+			
+			
+			//Para criar um SPAN onde o simbolo X sera colocada e Para criar o proprio simbolo e Para inserir remove em span2
+			var spanremove = document.createElement("SPAN");
+			var remove = document.createTextNode("\u00D7");			
+			spanremove.appendChild(remove);
+			
+			
+			tarefa.appendChild(spancheck);
 			tarefa.appendChild(texto);
-			   
-			var span = document.createElement("SPAN");
-			var check = document.createTextNode("\u2713");   
-			span.appendChild(check);
-			   
-			document.getElementById("lista").appendChild(span);
-
-			//Limpar o campo de tarefas 
+			tarefa.appendChild(spanremove);
+			 
+			 
+			//Inserir atraves da ID usando appenchild e Limpar o campo de tarefas Para acresentar um novo ao corpo o body
+			document.getElementById("lista").appendChild(tarefa);
 			document.getElementById("Criador").value = "";
-
-
-
-			//Para acresentar um novo ao corpo o body
 			document.body.appendChild(tarefa);
-			//return false;
+
 		}
 	}else{
 	return true;
 	}
-
 }
 
-function deletaTarefa(event){
-
-
-}
