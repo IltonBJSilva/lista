d1039f677c612d1a85233bc108f3ab0a2fb6edcb
IltonBJSilva <xlsonplay@gmail.com>
2018-11-22 17:23:40 -0200

    4 Implementação colocada antes da verificação do Francisco

--- script.js
+++ script.js
@@ -15,9 +15,19 @@ function acresentaTarefa(event){
 			var check = document.createTextNode("\u2713");
 			spancheck.appendChild(check);
 			spancheck.id = "correto";
-			spancheck.className = "estilo";
-			spancheck.addEventListener("click" function(){
-				tarefa.style.textDecoration = "line-through"
+			spancheck.className = "ativo";
+			spancheck.addEventListener("click", function(){
+				if(spancheck.className == "ativo"){
+				    spancheck.className = "desativada";
+					tarefa.className = "riscado";
+					
+		         } else if (spancheck.className == "desativada") {
+					 spancheck.className = "ativo";
+					 tarefa.className = "";
+					 
+					 
+					
+				}				
 			});
 			
 			//Para criar um SPAN onde o simbolo X sera colocada e Para criar o proprio simbolo e Para inserir remove em span2
@@ -25,7 +35,7 @@ function acresentaTarefa(event){
 			var remove = document.createTextNode("\u00D7");			
 			spanremove.appendChild(remove);
 			spanremove.id = "removedor"
-			spanremove.className = "estilo";
+			spanremove.className = "ativo";
 			spanremove.addEventListener("click", function(){	
     			tarefa.remove();
 			});
